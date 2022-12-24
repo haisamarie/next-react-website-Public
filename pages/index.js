5fe7751d85209c75aabb70a4809c818c8fe53881
Marie <marie.199204@gmail.com>
2022-12-24 14:48:36 +0900

    appコンポーネント作成

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
-import Header from 'component/header'
 import Hero from 'component/hero'
-import Footer from 'component/footer'
-import Layout from 'component/layout'
 
 export default function Home() {
-  return (
-    <>
-      <Layout>
-        <Hero />
-      </Layout>
-    </>
-  )
+  return <Hero />
 }
